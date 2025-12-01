ef31b5d210b01bea2823498e012822b893205545
Roberto Tambunan <robertotambunan14@gmail.com>
2025-12-01 22:13:59 +0700

    fix: spreadsheetid to original

--- app/daftar-pengungsi/page.tsx
+++ app/daftar-pengungsi/page.tsx
@@ -5,7 +5,7 @@ import { mapSheetDataEvacuee } from '../../utils/dataMapper';
 export const revalidate = 300;
 
 export default async function EvacueesPage() {
-  const spreadsheetId = '1eh29yJ4iDHpDgpn0WEaEje42bzWV5M6OHKxUo7F4KMc';
+  const spreadsheetId = '11lz-JRqZm7nRt1Ya4ARFPFv4MoMEn72G2ChoaBsewaI';
 
   const lastUpdate = await getSheetLastUpdate(spreadsheetId);
 
