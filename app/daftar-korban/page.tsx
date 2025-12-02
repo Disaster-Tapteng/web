6e353b6e7aa811bb35fd932f7bc3dde579afcf50
Roberto Tambunan <robertotambunan14@gmail.com>
2025-12-02 23:48:59 +0700

    feat: add evacuees data src + centralized spreadsheetid to env-var

--- app/daftar-korban/page.tsx
+++ app/daftar-korban/page.tsx
@@ -1,12 +1,11 @@
 import { DeceasedVictims } from '@/components/deceased_victims';
-import { getSheetData, getSheetLastUpdate } from '../../lib/sheet/google-sheets';
+import { getSheetData, getSheetLastUpdate, getSpreadsheetId } from '../../lib/sheet/google-sheets';
 import { mapSheetDataDeceased } from '@/utils/dataMapper';
 
 export const revalidate = 300;
 
 export default async function Home() {
-  const spreadsheetId = '11lz-JRqZm7nRt1Ya4ARFPFv4MoMEn72G2ChoaBsewaI';
-
+  const spreadsheetId = getSpreadsheetId();
   // Get last update time from spreadsheet file metadata
   const lastUpdate = await getSheetLastUpdate(spreadsheetId);
   const data = await getSheetData('DATA-MENINGGAL!A4:N', spreadsheetId);
--- app/daftar-pengungsi/page.tsx
+++ app/daftar-pengungsi/page.tsx
@@ -1,19 +1,21 @@
 import { Evacuees } from '@/components/evacuees';
-import { getSheetData, getSheetLastUpdate } from '../../lib/sheet/google-sheets';
+import { getSheetData, getSheetLastUpdate, getSpreadsheetId } from '../../lib/sheet/google-sheets';
 import { mapSheetDataEvacuee } from '../../utils/dataMapper';
 
 export const revalidate = 300;
 
 export default async function EvacueesPage() {
-  const spreadsheetId = '11lz-JRqZm7nRt1Ya4ARFPFv4MoMEn72G2ChoaBsewaI';
-
+  const spreadsheetId = getSpreadsheetId();
   const lastUpdate = await getSheetLastUpdate(spreadsheetId);
 
-  const [tukkaRaw, gorPandanRaw, hotelHasianRaw] = await Promise.all([
-    getSheetData('PENGUNSI-TUKKA!B7:B', spreadsheetId),
-    getSheetData('PENGUNSI-GOR PANDAN!B6:B', spreadsheetId),
-    getSheetData('PENGUNGSI-HOTEL HASIAN DAN CC!B8:B', spreadsheetId),
-  ]);
+  const [tukkaRaw, gorPandanRaw, hotelHasianRaw, jayaGreenRaw, sibuluanNauliRaw] =
+    await Promise.all([
+      getSheetData('PENGUNSI-TUKKA!B7:B', spreadsheetId),
+      getSheetData('PENGUNSI-GOR PANDAN!B6:B', spreadsheetId),
+      getSheetData('PENGUNGSI-HOTEL HASIAN DAN CC!B8:B', spreadsheetId),
+      getSheetData('PENGUNGSI-JAYA GREEN!B7:B', spreadsheetId),
+      getSheetData('PENGUNGSI-LINK 1 SIBULUAN NAULI!B2:B', spreadsheetId),
+    ]);
 
   const tukka = mapSheetDataEvacuee(tukkaRaw ?? [], 'Kecamatan Tukka');
   const gorPandan = mapSheetDataEvacuee(gorPandanRaw ?? [], 'Gedung Serba Guna Pandan');
@@ -21,8 +23,13 @@ export default async function EvacueesPage() {
     hotelHasianRaw ?? [],
     'Hotel Hasian / Pastoran Santo Yosef',
   );
+  const jayaGreen = mapSheetDataEvacuee(jayaGreenRaw ?? [], 'Jaya Green - Terminal Baru');
+  const sibuluanNauli = mapSheetDataEvacuee(
+    sibuluanNauliRaw ?? [],
+    'Huta Godang Kel. Sibuluan Nauli',
+  );
 
-  const initialData = [...tukka, ...gorPandan, ...hotelHasian];
+  const initialData = [...tukka, ...gorPandan, ...hotelHasian, ...jayaGreen, ...sibuluanNauli];
 
   return (
     <main className="min-h-screen bg-background">
